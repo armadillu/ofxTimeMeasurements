121eb3bab7902c662e7daca196965c8bc8086b3a
Oriol Ferrer Mesia <oriol@uri.cat>
2017-01-31 16:21:24 -0500

    clearer install sh

--- scripts/ci/linux/install.sh
+++ scripts/ci/linux/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Executing ci/linux/install.sh"
+
 export OF_ROOT=~/openFrameworks
 export OF_ADDONS=$OF_ROOT/addons
 
@@ -10,6 +12,7 @@ cd $OF_ADDONS
 
 for ADDON in $ADDONS
 do
+  echo "Cloning addon '$ADDON' to " `pwd`
   git clone --depth=1 --branch=$OF_BRANCH https://github.com/$ADDON.git
 done
 
--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -10,6 +10,7 @@ cd $OF_ADDONS
 
 for ADDON in $ADDONS
 do
+  echo "Cloning addon '$ADDON' to " `pwd`
   git clone --depth=1 --branch=$OF_BRANCH https://github.com/$ADDON.git
 done
 

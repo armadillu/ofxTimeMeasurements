78a7b9b66ceb3988c127648ee41a3ea0e282d312
Oriol Ferrer Mesia <oriol@uri.cat>
2017-06-19 11:58:47 -0400

    meh

--- src/ofxTimeMeasurements.cpp
+++ src/ofxTimeMeasurements.cpp
@@ -1700,11 +1700,11 @@ void ofxTimeMeasurements::drawSmoothFpsClock(float x, float y, float radius){
 	static float r = 0;
 	ofPushMatrix();
 	ofTranslate(x, y);
-#if (OF_VERSION_MINOR <= 9)
+	#if (OF_VERSION_MINOR <= 9)
 	ofRotate(r, 0, 0, 1);
-#else
+	#else
 	ofRotateDeg(r, 0, 0, 1);
-#endif
+	#endif
 	float s = radius * 0.05;
 	ofDrawRectangle(-s * 0.5f, - 0.5f * s, radius, s);
 	ofPopMatrix();
